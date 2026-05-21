b93ae6a1a48765b1fdb3a6c4cabcfe11a68651f1
Gaurav Patrekar <gauravpatrekar01@gmail.com>
2026-05-21 23:37:51 +0530

    Fix environment configuration

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
+        "https://gaurav-patrekar-portfolio.vercel.app/",
         "http://localhost:5173",
         "http://localhost:8080",
         "http://127.0.0.1:5173",
